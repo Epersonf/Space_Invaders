16a23136eeea10237fb2d3fe574634495e627dd1
epersonf <epersoncmxf@gmail.com>
2019-09-14 02:00:32 -0300

    Stars added and fire.

--- Game/menu.py
+++ Game/menu.py
@@ -1,4 +1,6 @@
+from Game import stars_ambient
 from Game.button import *
+from Game.stars_ambient import *
 
 class Main_Menu:
 
@@ -7,6 +9,7 @@ class Main_Menu:
     Mode = None
     Ranking = None
     Exit = None
+    Stars_Amb = None
 
     def __init__(self, gui):
         self.gui = gui
@@ -14,10 +17,12 @@ class Main_Menu:
         self.Mode = Btn("Assets/BTN.png", "Mode", gui, gui.width / 2 - 200, gui.height / 2 + 100)
         self.Ranking = Btn("Assets/BTN.png", "Rank", gui, gui.width / 2 - 200, gui.height / 2 + 200)
         self.Exit = Btn("Assets/BTN.png", "Exit", gui, gui.width / 2 - 200, gui.height / 2 + 300)
+        self.Stars_Amb = Stars(gui)
 
 
     def draw(self, level, mode):
         self.gui.set_background_color((0, 0, 0))
+        self.Stars_Amb.draw()
         if self.Play.draw():
             level[0] = 3
         if self.Mode.draw():
--- Game/stars_ambient.py
+++ Game/stars_ambient.py
@@ -1,2 +1,27 @@
-def show_stars(gui):
-    gui.set_background_color([0, 0, 0])
+from Game.PPlay.gameimage import *
+from random import randint
+
+class Stars:
+
+    gui = None
+    stars_obj = None
+    vel = None
+
+    def __init__(self, gui, amount=2000):
+        self.gui = gui
+        self.stars_obj = [None] * amount
+        self.vel = [None] * amount
+        for i in range(amount):
+            self.stars_obj[i] = GameImage("Assets/STAR.png")
+            self.stars_obj[i].x = randint(0, self.gui.width)
+            self.stars_obj[i].y = self.gui.height
+            self.vel[i] = randint(0, 300)
+
+    def draw(self):
+        for i in range(len(self.stars_obj)):
+            self.stars_obj[i].y -= self.vel[i] * self.gui.delta_time()
+            if self.stars_obj[i].y <= 0:
+                self.stars_obj[i].x = randint(0, self.gui.width)
+                self.stars_obj[i].y = self.gui.height
+                self.vel[i] = randint(20, 300)
+            self.stars_obj[i].draw()
